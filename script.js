f615420060ae61352230b1609c8c14b4ac480617
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-08-05 16:35:24 -0400

    add button to change size of grid

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('.button')
 
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square')
-    container.appendChild(square)
+let gridSize = 100
+
+function updateGridSize(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        let row = document.createElement('div')
+        row.classList.add('row')
+        container.appendChild(row)
+        console.log(row)
+        for (let j = 0; j< gridSize; j++) {
+            let square = document.createElement('div');
+            square.classList.add('square')
+            row.appendChild(square)
+            console.log(square)
+        }
+    }
+}
+
+
+button.onclick = function getGridSize() {
+    gridSize = parseInt(prompt('Please provide a number from 10 to 100'));  // gotta check a better way of doing this and commit
+    if (gridSize >= 10 && gridSize <= 100) {
+        
+    } else {
+        alert('insert a valid value')
+    }
 };
+
+updateGridSize(gridSize)
 
 const squares = document.querySelectorAll('.square');
 
